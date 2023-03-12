54b5a4ab4572500c8ffb93584ae3359061570b55
NettoH3R <jovinettonetto@gmail.com>
2023-03-12 14:30:13 -0300

    Save 1.0

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-fefef
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Login</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">
+            
+        </label>
+
+    </form>
+</body>
+</html>
